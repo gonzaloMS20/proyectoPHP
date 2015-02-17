7616db6ec8492f2d83f4b5bcb164412f0bcf2df8
root <root@debian.gmartinez.local>
2015-02-17 04:29:14 -0600

    hola

--- tienda.sql
+++ tienda.sql
@@ -8,14 +8,15 @@ create table producto(
 id_producto serial primary key,
 cantidad int not null,
 precio numeric(7,2) not null,
-nombre char(50) not null,
+nombre varchar(50) not null,
 imagen OID not null
 );
+
 create table usuario(
 id_usuario serial primary key,
-nombre char(30) not null,
-ap_paterno char(30) not null,
-ap_materno char(30) not null,
+nombre varchar(30) not null,
+ap_paterno varchar(30) not null,
+ap_materno varchar(30) not null,
 username varchar(30) unique not null,
 password varchar(32) not null
 );
