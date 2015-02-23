0ea60c50c37ced1eae633f6674de806fa0d2997f
leti <root@php.php.unam.cert>
2015-02-23 01:16:25 -0600

    commbo

---
+++ mascotas.php
@@ -0,0 +1,28 @@
+<?php 
+
+Class Usuario{
+
+	var $idUsuario;
+	var $nombre;
+	var $paPaterno;
+	var $apMaterno;
+	var $userName;
+	var $password;
+	
+}
+
+?>
+<select name="hall" id="hall" value"3">
+	<option>conejo</option>
+	<option>perro</option>
+	<option>loro</option>
+	<option>gato</option>
+	<option>hamster</option>
+</select>	
+<select name="num" id="num" value"1">
+	<option>1</option>
+	<option>2</option>
+	<option>3</option>
+	<option>4</option>
+	<option>5</option>
+</select>	
