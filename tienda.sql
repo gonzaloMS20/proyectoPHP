c48caf65e9a569d2a11d61b2f547f118ad052a64
leticia <root@php.php.unam.cert>
2015-02-22 23:14:38 -0600

    tienda

--- tienda.sql
+++ tienda.sql
@@ -1,21 +1,19 @@
---#sudo - postgres
---#drop database if exists producto;
-
---create database producto;
 
 \c tienda
 create table producto(
 id_producto serial primary key,
 cantidad int not null,
 precio numeric(7,2) not null,
-nombre char(50) not null,
+--nombre varchar(50) not null,
+nombre text not null,
+descripcion text not null,
 imagen OID not null
 );
 create table usuario(
 id_usuario serial primary key,
 nombre char(30) not null,
-ap_paterno char(30) not null,
-ap_materno char(30) not null,
+ap_paterno varchar(30) not null,
+ap_materno varchar(30) not null,
 username varchar(30) unique not null,
 password varchar(32) not null
 );
@@ -28,3 +26,5 @@ foreign key (id_usuario) references usuario (id_usuario),
 foreign key (id_producto) references producto (id_producto)
 );
 
+insert into producto (id_producto,cantidad,precio,nombre,descripcion,imagen) 
+values (1,100,18000.50,conejo,"todos con 30 dias de nacidos",lo_import('/home/php/imagen.png'));
